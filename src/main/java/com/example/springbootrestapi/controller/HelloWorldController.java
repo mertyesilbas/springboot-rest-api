0b127579c4f4f97fc2f09db2b4e63c05d11e1475
Mert Yeşilbaş <mert.yesilbas.888@hotmail.com>
2023-09-26 18:33:51 +0300

    Created Hello World Controller

--- src/main/java/com/example/springbootrestapi/controller/HelloWorldController.java
+++ src/main/java/com/example/springbootrestapi/controller/HelloWorldController.java
@@ -1,5 +1,16 @@
 package com.example.springbootrestapi.controller;
 
-public class HelloWorldController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HelloWorldController {
+    // HTTP GET request
+    // http://localhost:8080/hello-world
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
